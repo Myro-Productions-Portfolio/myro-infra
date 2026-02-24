9ca18fc7bee9c33299ff5184293feeb625e9f72d
Myro Productions <husky2466@gmail.com>
2026-02-24 12:00:50 -0500

    feat(infra): add custome domain and ACM certificate to CloudFront

--- variables.tf
+++ variables.tf
@@ -13,5 +13,11 @@ variable "project_name" {
 variable "domain_name" {
   description = "Optional custom domain name (leave empty to use CloudFront URL)"
   type        = string
-  default     = ""
+  default     = "devops.myroproductions.com"
+}
+
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate for the custom domain (must be in us-east-1)"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:498618930321:certificate/c2c23f27-4435-4fe1-8671-b97141a98a72"
 }
